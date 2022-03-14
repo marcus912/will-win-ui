ac0aa6e61bbb616aa43426bb3cf4401befc0f566
Marcus Lin <waterbalance2000@gmail.com>
2022-03-15 00:14:47 +0800

    Load data into Data grid.

--- src/views/purchase/PurchaseItem.js
+++ src/views/purchase/PurchaseItem.js
@@ -1,230 +1,58 @@
-import React from 'react';
 import MainCard from '../../ui-component/cards/MainCard';
-import PropTypes from 'prop-types';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import AddIcon from '@mui/icons-material/Add';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/DeleteOutlined';
-import SaveIcon from '@mui/icons-material/Save';
-import CancelIcon from '@mui/icons-material/Close';
-import { DataGridPro, GridActionsCellItem, GridToolbarContainer, useGridApiRef } from '@mui/x-data-grid-pro';
-import { randomCreatedDate, randomId, randomTraderName, randomUpdatedDate } from '@mui/x-data-grid-generator';
+import { DataGrid } from '@mui/x-data-grid';
+import Alert from '@mui/material/Alert';
+import { useCallback, useState } from 'react';
+import { mockRows } from './Random';
 
-const rows = [
+const columns = [
+  { field: 'name', headerName: 'Name', width: 180, editable: true },
+  { field: 'age', headerName: 'Age', type: 'number', editable: true },
   {
-    id: randomId(),
-    name: randomTraderName(),
-    age: 25,
-    dateCreated: randomCreatedDate(),
-    lastLogin: randomUpdatedDate()
+    field: 'dateCreated',
+    headerName: 'Date Created',
+    type: 'date',
+    width: 180,
+    editable: true
   },
   {
-    id: randomId(),
-    name: randomTraderName(),
-    age: 36,
-    dateCreated: randomCreatedDate(),
-    lastLogin: randomUpdatedDate()
-  },
-  {
-    id: randomId(),
-    name: randomTraderName(),
-    age: 19,
-    dateCreated: randomCreatedDate(),
-    lastLogin: randomUpdatedDate()
-  },
-  {
-    id: randomId(),
-    name: randomTraderName(),
-    age: 28,
-    dateCreated: randomCreatedDate(),
-    lastLogin: randomUpdatedDate()
-  },
-  {
-    id: randomId(),
-    name: randomTraderName(),
-    age: 23,
-    dateCreated: randomCreatedDate(),
-    lastLogin: randomUpdatedDate()
+    field: 'lastLogin',
+    headerName: 'Last Login',
+    type: 'dateTime',
+    width: 220,
+    editable: true
   }
 ];
 
-const PurchaseItem = () => {
-  const handleClick = () => {
-    const id = randomId();
-    apiRef.current.updateRows([{ id, isNew: true }]);
-    apiRef.current.setRowMode(id, 'edit');
-    // Wait for the grid to render with the new row
-    setTimeout(() => {
-      apiRef.current.scrollToIndexes({
-        rowIndex: apiRef.current.getRowsCount() - 1
-      });
+const PurchaseItem = () => (
+  <MainCard title='進貨品名'>
+    <RowEditControlGrid />
+  </MainCard>
+);
 
-      apiRef.current.setCellFocus(id, 'name');
-    });
-  };
+function RowEditControlGrid() {
+  const [editRowsModel, setEditRowsModel] = useState({});
+
+  const handleEditRowsModelChange = useCallback((model) => {
+    setEditRowsModel(model);
+  }, []);
 
   return (
-    <MainCard title='進貨品名'>
-      <FullFeaturedCrudGrid />
-    </MainCard>
-  );
-};
-
-function EditToolbar(props) {
-  const { apiRef } = props;
-
-  const handleClick = () => {
-    const id = randomId();
-    apiRef.current.updateRows([{ id, isNew: true }]);
-    apiRef.current.setRowMode(id, 'edit');
-    // Wait for the grid to render with the new row
-    setTimeout(() => {
-      apiRef.current.scrollToIndexes({
-        rowIndex: apiRef.current.getRowsCount() - 1
-      });
-
-      apiRef.current.setCellFocus(id, 'name');
-    });
-  };
-
-  return (
-    <GridToolbarContainer>
-      <Button color='primary' startIcon={<AddIcon />} onClick={handleClick}>
-        Add record
-      </Button>
-    </GridToolbarContainer>
-  );
-}
-
-EditToolbar.propTypes = {
-  apiRef: PropTypes.shape({
-    current: PropTypes.object.isRequired
-  }).isRequired
-};
-
-function FullFeaturedCrudGrid() {
-  const apiRef = useGridApiRef();
-
-  const handleRowEditStart = (params, event) => {
-    event.defaultMuiPrevented = true;
-  };
-
-  const handleRowEditStop = (params, event) => {
-    event.defaultMuiPrevented = true;
-  };
-
-  const handleCellFocusOut = (params, event) => {
-    event.defaultMuiPrevented = true;
-  };
-
-  const handleEditClick = (id) => (event) => {
-    event.stopPropagation();
-    apiRef.current.setRowMode(id, 'edit');
-  };
-
-  const handleSaveClick = (id) => async (event) => {
-    event.stopPropagation();
-    // Wait for the validation to run
-    const isValid = await apiRef.current.commitRowChange(id);
-    if (isValid) {
-      apiRef.current.setRowMode(id, 'view');
-      const row = apiRef.current.getRow(id);
-      apiRef.current.updateRows([{ ...row, isNew: false }]);
-    }
-  };
-
-  const handleDeleteClick = (id) => (event) => {
-    event.stopPropagation();
-    apiRef.current.updateRows([{ id, _action: 'delete' }]);
-  };
-
-  const handleCancelClick = (id) => (event) => {
-    event.stopPropagation();
-    apiRef.current.setRowMode(id, 'view');
-
-    const row = apiRef.current.getRow(id);
-    if (row.isNew) {
-      apiRef.current.updateRows([{ id, _action: 'delete' }]);
-    }
-  };
-
-  const columns = [
-    { field: 'name', headerName: 'Name', width: 180, editable: true },
-    { field: 'age', headerName: 'Age', type: 'number', editable: true },
-    {
-      field: 'dateCreated',
-      headerName: 'Date Created',
-      type: 'date',
-      width: 180,
-      editable: true
-    },
-    {
-      field: 'lastLogin',
-      headerName: 'Last Login',
-      type: 'dateTime',
-      width: 220,
-      editable: true
-    },
-    {
-      field: 'actions',
-      type: 'actions',
-      headerName: 'Actions',
-      width: 100,
-      cellClassName: 'actions',
-      getActions: ({ id }) => {
-        const isInEditMode = apiRef.current.getRowMode(id) === 'edit';
-
-        if (isInEditMode) {
-          return [
-            <GridActionsCellItem icon={<SaveIcon />} label='Save' onClick={handleSaveClick(id)} color='primary' />,
-            <GridActionsCellItem
-              icon={<CancelIcon />}
-              label='Cancel'
-              className='textPrimary'
-              onClick={handleCancelClick(id)}
-              color='inherit'
-            />
-          ];
-        }
-
-        return [
-          <GridActionsCellItem icon={<EditIcon />} label='Edit' className='textPrimary' onClick={handleEditClick(id)} color='inherit' />,
-          <GridActionsCellItem icon={<DeleteIcon />} label='Delete' onClick={handleDeleteClick(id)} color='inherit' />
-        ];
-      }
-    }
-  ];
-
-  return (
-    <Box
-      sx={{
-        height: 500,
-        width: '100%',
-        '& .actions': {
-          color: 'text.secondary'
-        },
-        '& .textPrimary': {
-          color: 'text.primary'
-        }
-      }}
-    >
-      <DataGridPro
-        rows={rows}
-        columns={columns}
-        apiRef={apiRef}
-        editMode='row'
-        onRowEditStart={handleRowEditStart}
-        onRowEditStop={handleRowEditStop}
-        onCellFocusOut={handleCellFocusOut}
-        components={{
-          Toolbar: EditToolbar
-        }}
-        componentsProps={{
-          toolbar: { apiRef }
-        }}
-      />
-    </Box>
+    <div style={{ width: '100%' }}>
+      <div style={{ height: '60vh', width: '100%' }}>
+        <DataGrid
+          rows={mockRows}
+          columns={columns}
+          editRowsModel={editRowsModel}
+          editMode='row'
+          onEditRowsModelChange={handleEditRowsModelChange}
+          autoPageSize
+          rowHeight={40}
+        />
+      </div>
+      <Alert severity='info' style={{ marginTop: 8 }}>
+        <code>editRowsModel: {JSON.stringify(editRowsModel)}</code>
+      </Alert>
+    </div>
   );
 }
 
