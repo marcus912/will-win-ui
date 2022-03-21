d957d7c20eb04e8e326a8960583d5fee8b172c32
Marcus Lin <waterbalance2000@gmail.com>
2022-03-22 01:34:13 +0800

    Support data input

--- src/views/purchase/item/PurchaseItemDialogForm.js
+++ src/views/purchase/item/PurchaseItemDialogForm.js
@@ -1,34 +1,53 @@
-import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@mui/material';
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField, Typography } from '@mui/material';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+import debug from 'debug';
 
-export const PurchaseItemDialogForm = ({ open = false, setOpen }) => (
-  <div>
-    <Dialog open={open} onClose={() => setOpen(false)}>
-      <DialogTitle>新增品名</DialogTitle>
-      <DialogContent>
-        <Box
-          component="form"
-          sx={{
-            '& .MuiTextField-root': { m: 1, width: '25ch' }
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <div>
-            <TextField required id="name" label="品名" />
-          </div>
-          <div>
-            <TextField id="comment" label="註解" multiline rows={4} />
-          </div>
-        </Box>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={() => setOpen(false)}>Cancel</Button>
-        <Button onClick={() => setOpen(false)}>Submit</Button>
-      </DialogActions>
-    </Dialog>
-  </div>
-);
+const logger = debug('ww:purchase-item-dialog-form');
+
+export const PurchaseItemDialogForm = ({ open = false, setOpen }) => {
+  const [values, setValues] = useState({
+    name: '',
+    comment: '',
+    status: ''
+  });
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+  return (
+    <div>
+      <Dialog fullWidth open={open} onClose={() => setOpen(false)}>
+        <DialogTitle>新增品名</DialogTitle>
+        <DialogContent>
+          <Box
+            sx={{
+              '& .MuiTextField-root': { my: 1 }
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <div>
+              <TextField fullWidth required id="name" label="品名" onChange={handleChange('name')} />
+            </div>
+            <div>
+              <TextField fullWidth id="comment" label="註解" multiline rows={4} onChange={handleChange('comment')} />
+            </div>
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpen(false)}>Cancel</Button>
+          <Button
+            onClick={() => {
+              logger('Submit data: ', values);
+            }}
+          >
+            Submit
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
 
 PurchaseItemDialogForm.propTypes = {
   open: PropTypes.bool,
