76b20be811948869087c8724a8057f0f217da4b5
Feng <hsufengs@gmail.com>
2023-11-27 21:31:49 +0800

    add formik

--- src/views/willwin/material/materialDialog.js
+++ src/views/willwin/material/materialDialog.js
@@ -2,30 +2,43 @@ import React from 'react';
 
 // material-ui
 import { useTheme } from '@mui/material/styles';
+import { Formik } from 'formik';
 import {
+    Box,
     Button,
     Dialog,
     DialogActions,
     DialogContent,
     DialogContentText,
     DialogTitle,
-    CardContent,
+    FormControl,
     Grid,
+    InputLabel,
+    OutlinedInput,
     Stack,
-    TextField,
+    Select,
+    MenuItem,
     Typography,
     IconButton
 } from '@mui/material';
 
 import EditTwoToneIcon from '@mui/icons-material/EditTwoTone';
-import InputLabel from 'ui-component/extended/Form/InputLabel';
-import FormControlSelect from 'ui-component/extended/Form/FormControlSelect';
+import AnimateButton from 'ui-component/extended/AnimateButton';
+
 import axios from 'axios';
 // ===============================|| UI DIALOG - FORMS ||=============================== //
 
-export default function FormDialog({row}) {
+export default function FormDialog({ row, ...others }) {
     const theme = useTheme();
     const [open, setOpen] = React.useState(false);
+    const [status, setStatus] = React.useState(10);
+
+    const statusChange = (event) => {
+        console.log(event.target.value);
+        setStatus(event.target.value);
+        console.log(typeof age);
+    };
+
     const handleClickOpen = () => {
         setOpen(true);
     };
@@ -34,31 +47,38 @@ export default function FormDialog({row}) {
         setOpen(false);
     };
 
-    const currencies = [
-        {
-            value: '1',
-            label: 'Complete'
-        },
-        {
-            value: '2',
-            label: 'Pending'
-        },
-        {
-            value: '3',
-            label: 'Processing'
-        }
-    ];
+    // const currencies = [
+    //     {
+    //         value: '1',
+    //         label: 'Complete'
+    //     },
+    //     {
+    //         value: '2',
+    //         label: 'Pending'
+    //     },
+    //     {
+    //         value: '3',
+    //         label: 'Processing'
+    //     }
+    // ];
 
     //console.log(row);
     const callPut = () => {
-        axios.put(`https://polls.apiblueprint.org/purchase/${row.id}`, row);
-    }
+        try {
+            axios.put(`https://private-1baef-willwin.apiary-mock.com/material/${row.id}`, row);
+            // success, close dialog.
+            // here
+        } catch (e) {
+            // failed
+            console.log(e);
+        }
+    };
     return (
         <div>
             <IconButton color="secondary" size="large" aria-label="edit" onClick={handleClickOpen}>
                 <EditTwoToneIcon sx={{ fontSize: '1.3rem' }} />
             </IconButton>
-            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+            <Dialog fullWidth open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
                 {open && (
                     <>
                         <DialogTitle id="form-dialog-title">材料清單修改</DialogTitle>
@@ -69,43 +89,95 @@ export default function FormDialog({row}) {
                                         這邊新增備註
                                     </Typography>
                                 </DialogContentText>
-                                <CardContent>
-                                    <Grid container spacing={2} alignItems="center">
-                                        <Grid item xs={12}>
-                                            <InputLabel>ID: {row?.id}</InputLabel>
-                                        </Grid>
-                                        <Grid item xs={6}>
-                                            <InputLabel>材料名稱</InputLabel>
-                                            <TextField fullWidth placeholder="Enter Material" defaultValue={row?.name} />
-                                        </Grid>
-                                        <Grid item xs={12}>
-                                            <InputLabel>備註</InputLabel>
-                                            <TextField
-                                                fullWidth
-                                                id="outlined-multiline-flexible"
-                                                label="Comment"
-                                                multiline
-                                                rows={3}
-                                                defaultValue={row?.comment}
-                                            />
-                                        </Grid>
-                                        <Grid item xs={12}>
-                                            <InputLabel>狀態</InputLabel>
-                                            <FormControlSelect currencies={currencies} selected={row?.status} captionLabel="Status" />  
+                                <Formik
+                                    initialValues={{
+                                        material: '123',
+                                        status: status,
+                                        comment: '備註欄',
+                                        submit: null
+                                    }}
+                                    // validationSchema={Yup.object().shape({
+                                    //     email: Yup.string().email('Must be a valid email').max(255).required('Email is required'),
+                                    //     password: Yup.string().max(255).required('Password is required')
+                                    // })}
+                                    onSubmit={async (values) => {
+                                        console.log(values);
+                                    }}
+                                >
+                                    {({ handleBlur, handleChange, handleSubmit, isSubmitting, values }) => (
+                                        <form noValidate onSubmit={handleSubmit} {...others}>
+                                            <Grid container alignItems="center" justifyContent="space-between">
+                                                <Grid item xs={5}>
+                                                    <FormControl fullWidth>
+                                                        <InputLabel htmlFor="material">品名</InputLabel>
+                                                        <OutlinedInput
+                                                            id="material"
+                                                            type="text"
+                                                            value={values.material}
+                                                            name="material"
+                                                            onBlur={handleBlur}
+                                                            onChange={handleChange}
+                                                            inputProps={{}}
+                                                        />
+                                                    </FormControl>
+                                                </Grid>
+                                                <Grid item xs={5}>
+                                                    <FormControl fullWidth>
+                                                        <InputLabel htmlFor="status">狀態</InputLabel>
+                                                        <Select
+                                                            labelId="status"
+                                                            id="status"
+                                                            value={values.status}
+                                                            label="狀態"
+                                                            onChange={statusChange}
+                                                        >
+                                                            <MenuItem value={10}>Ten</MenuItem>
+                                                            <MenuItem value={20}>Twenty</MenuItem>
+                                                            <MenuItem value={30}>Thirty</MenuItem>
+                                                        </Select>
+                                                    </FormControl>
+                                                </Grid>
+                                            </Grid>
 
-                                        </Grid>
-                                    </Grid>
-                                </CardContent>
+                                            <FormControl fullWidth sx={{ mt: 2 }}>
+                                                <InputLabel htmlFor="comment">備註</InputLabel>
+                                                <OutlinedInput
+                                                    id="comment"
+                                                    type="text"
+                                                    value={values.comment}
+                                                    name="comment"
+                                                    onBlur={handleBlur}
+                                                    onChange={handleChange}
+                                                    inputProps={{}}
+                                                />
+                                            </FormControl>
+                                            <Box sx={{ mt: 2 }}>
+                                                <AnimateButton>
+                                                    <Button
+                                                        color="secondary"
+                                                        disabled={isSubmitting}
+                                                        fullWidth
+                                                        size="large"
+                                                        type="submit"
+                                                        variant="contained"
+                                                    >
+                                                        Sign In
+                                                    </Button>
+                                                </AnimateButton>
+                                            </Box>
+                                        </form>
+                                    )}
+                                </Formik>
+                                <DialogActions sx={{ pr: 2.5 }}>
+                                    <Button sx={{ color: theme.palette.error.dark }} onClick={handleClose} color="secondary">
+                                        取消
+                                    </Button>
+                                    <Button type="submit" variant="contained" size="small" onClick={callPut}>
+                                        修改
+                                    </Button>
+                                </DialogActions>
                             </Stack>
                         </DialogContent>
-                        <DialogActions sx={{ pr: 2.5 }}>
-                            <Button sx={{ color: theme.palette.error.dark }} onClick={handleClose} color="secondary">
-                                取消
-                            </Button>
-                            <Button variant="contained" size="small" onClick={callPut}>
-                                修改
-                            </Button>
-                        </DialogActions>
                     </>
                 )}
             </Dialog>
