6bcb2f871a234191f927c4001c2d2b3413a47217
Feng <hsufengs@gmail.com>
2023-12-19 15:43:42 +0800

    MaterialList Comp create

--- src/views/willwin/material/materialList.js
+++ src/views/willwin/material/materialList.js
@@ -1,42 +1,17 @@
-import PropTypes from 'prop-types';
 import * as React from 'react';
 
 // material-ui
 import { useTheme } from '@mui/material/styles';
-import {
-    Box,
-    CardContent,
-    Grid,
-    IconButton,
-    InputAdornment,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TablePagination,
-    TableRow,
-    TableSortLabel,
-    TextField,
-    Tooltip,
-    Typography
-} from '@mui/material';
-import { visuallyHidden } from '@mui/utils';
+import { IconButton, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from '@mui/material';
 
 // project imports
 import Chip from 'ui-component/extended/Chip';
-import MainCard from 'ui-component/cards/MainCard';
-import FormDialog from './materialDialog'
+import EnhancedTableHead from './EnhancedTableHead';
 import { useDispatch, useSelector } from 'store';
-import { getMaterials } from 'store/slices/material';
+import { getMaterials, setDialogRow } from 'store/slices/material';
 
 // assets
-import FilterListIcon from '@mui/icons-material/FilterListTwoTone';
-import PrintIcon from '@mui/icons-material/PrintTwoTone';
-import FileCopyIcon from '@mui/icons-material/FileCopyTwoTone';
-import SearchIcon from '@mui/icons-material/Search';
 import EditTwoToneIcon from '@mui/icons-material/EditTwoTone';
-
 
 // table sort
 function descendingComparator(a, b, orderBy) {
@@ -62,148 +37,23 @@ function stableSort(array, comparator) {
     return stabilizedThis.map((el) => el[0]);
 }
 
-// table header options
-
-const headCells = [
-    {
-        id: 'id',
-        numeric: true,
-        label: 'ID',
-        align: 'center'
-    },
-    {
-        id: 'name',
-        numeric: false,
-        label: '品名',
-        align: 'left'
-    },
-    {
-        id: 'type',
-        numeric: true,
-        label: '備註',
-        align: 'left'
-    },
-    {
-        id: 'status',
-        numeric: false,
-        label: '狀態',
-        align: 'center'
-    }
-];
-
-// ==============================|| TABLE HEADER ||============================== //
-
-function EnhancedTableHead({ order, orderBy, numSelected, onRequestSort, theme }) {
-    const createSortHandler = (property) => (event) => {
-        onRequestSort(event, property);
-    };
-
-    return (
-        <TableHead>
-            <TableRow>
-                {numSelected <= 0 &&
-                    headCells.map((headCell) => (
-                        <TableCell
-                            key={headCell.id}
-                            align={headCell.align}
-                            padding={headCell.disablePadding ? 'none' : 'normal'}
-                            sortDirection={orderBy === headCell.id ? order : false}
-                        >
-                            <TableSortLabel
-                                active={orderBy === headCell.id}
-                                direction={orderBy === headCell.id ? order : 'asc'}
-                                onClick={createSortHandler(headCell.id)}
-                            >
-                                {headCell.label}
-                                {orderBy === headCell.id ? (
-                                    <Box component="span" sx={visuallyHidden}>
-                                        {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                                    </Box>
-                                ) : null}
-                            </TableSortLabel>
-                        </TableCell>
-                    ))}
-                {numSelected <= 0 && (
-                    <TableCell sortDirection={false} align="center" sx={{ pr: 3 }}>
-                        <Typography variant="subtitle1" sx={{ color: theme.palette.mode === 'dark' ? 'grey.600' : 'grey.900' }}>
-                            Edit
-                        </Typography>
-                    </TableCell>
-                )}
-            </TableRow>
-        </TableHead>
-    );
-}
-
-EnhancedTableHead.propTypes = {
-    theme: PropTypes.object,
-    selected: PropTypes.array,
-    numSelected: PropTypes.number.isRequired,
-    onRequestSort: PropTypes.func.isRequired,
-    onSelectAllClick: PropTypes.func.isRequired,
-    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
-    orderBy: PropTypes.string.isRequired,
-    rowCount: PropTypes.number.isRequired
-};
-
 // ==============================|| Material LIST ||============================== //
 
-const MaterialList = () => {
+const MaterialList = ({ ...others }) => {
     const theme = useTheme();
     const dispatch = useDispatch();
     const [order, setOrder] = React.useState('asc');
     const [orderBy, setOrderBy] = React.useState('calories');
     const [selected, setSelected] = React.useState([]);
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(5);
-    const [search, setSearch] = React.useState('');
-    const [rows, setRows] = React.useState([]);
-    const [filter, setFilter] = React.useState([])
-    const [open, setOpen] = React.useState(false);
-    const [dialogRow, setDialogRow] = React.useState([])
-    const { materiallist, isLoaded } = useSelector((state) => state.material);
-    
+    const {  isLoaded } = useSelector((state) => state.material);
+    const {  page, rowsPerPage, rows, setOpen } = others;
+
     //console.log(isLoaded);
-    React.useEffect(() => {
-        setRows(materiallist);
-    }, [materiallist]);
-    React.useEffect(() => {
-        setFilter(materiallist);
-    }, [materiallist]);
     React.useEffect(() => {
         if (isLoaded == false) {
             dispatch(getMaterials());
-            
         }
     }, [isLoaded]);
-    const handleSearch = (event) => {
-        const newString = event?.target.value;
-        setSearch(newString || '');
-
-        if (newString) {
-            const newRows = filter.filter((row) => {
-                let matches = true;
-
-                const properties = [ 'id','name', 'comment', 'status'];
-                let containsQuery = false;
-
-                properties.forEach((property) => {
-                    if (row[property].toString().toLowerCase().includes(newString.toString().toLowerCase())) {
-                        containsQuery = true;
-                    }
-                });
-
-                if (!containsQuery) {
-                    matches = false;
-                }
-
-                return matches;
-            });
-            setRows(newRows);
-        } else {
-            setRows(materiallist);
-        }
-    };
 
     const handleRequestSort = (event, property) => {
         const isAsc = orderBy === property && order === 'asc';
@@ -241,170 +91,100 @@ const MaterialList = () => {
         setSelected(newSelected);
     };
 
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
-    };
-
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(parseInt(event?.target.value, 10));
-        setPage(0);
-    };
-
     const isSelected = (name) => selected.indexOf(name) !== -1;
     const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
 
     const handleClickOpen = (row) => {
         setOpen(true);
-        setDialogRow(row);
-        //console.log(row)
-      };
-    
-    const handleClose = () => {
-        setOpen(false);
-    };
+        dispatch(setDialogRow(row));
+    }; 
 
-    
     return (
-        <MainCard  content={false}>
-            <CardContent>
-                <Grid container justifyContent="space-between" alignItems="center" spacing={2}>
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                            InputProps={{
-                                startAdornment: (
-                                    <InputAdornment position="start">
-                                        <SearchIcon fontSize="small" />
-                                    </InputAdornment>
-                                )
-                            }}
-                            onChange={handleSearch}
-                            placeholder="Search Order"
-                            value={search}
-                            size="small"
-                        />
-                    </Grid>
-                    <Grid item xs={12} sm={6} sx={{ textAlign: 'right' }}>
-                        <Tooltip title="Copy">
-                            <IconButton size="large">
-                                <FileCopyIcon />
-                            </IconButton>
-                        </Tooltip>
-                        <Tooltip title="Print">
-                            <IconButton size="large">
-                                <PrintIcon />
-                            </IconButton>
-                        </Tooltip>
-                        <Tooltip title="Filter">
-                            <IconButton size="large">
-                                <FilterListIcon />
-                            </IconButton>
-                        </Tooltip>
-                    </Grid>
-                </Grid>
-            </CardContent>
-
-            {/* table */}
-            <TableContainer>
-                <Table sx={{ minWidth: 750 }} aria-labelledby="tableTitle">
-                    <EnhancedTableHead
-                        numSelected={selected.length}
-                        order={order}
-                        orderBy={orderBy}
-                        onSelectAllClick={handleSelectAllClick}
-                        onRequestSort={handleRequestSort}
-                        rowCount={rows.length}
-                        theme={theme}
-                        selected={selected}
-                    />
-                    <TableBody>
-                        {stableSort(rows, getComparator(order, orderBy))
-                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                            .map((row, index) => {
-                                /** Make sure no display bugs if row isn't an OrderData object */
-                                if (typeof row === 'number') return null;
-
-                                const isItemSelected = isSelected(row.materialname);
-                                const labelId = `enhanced-table-checkbox-${index}`;
-
-                                //const newLocal = <SlideDialog row={row} />;
-                                return (
-                                    <TableRow
-                                        hover
-                                        role="checkbox"
-                                        aria-checked={isItemSelected}
-                                        tabIndex={-1}
-                                        key={index}
-                                        selected={isItemSelected}
+        <TableContainer>
+            <Table sx={{ minWidth: 750 }} aria-labelledby="tableTitle">
+                <EnhancedTableHead
+                    numSelected={selected.length}
+                    order={order}
+                    orderBy={orderBy}
+                    onSelectAllClick={handleSelectAllClick}
+                    onRequestSort={handleRequestSort}
+                    rowCount={rows.length}
+                    theme={theme}
+                    selected={selected}
+                />
+                <TableBody>
+                    {stableSort(rows, getComparator(order, orderBy))
+                        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                        .map((row, index) => {
+                            /** Make sure no display bugs if row isn't an OrderData object */
+                            if (typeof row === 'number') return null;
+                            const isItemSelected = isSelected(row.materialname);
+                            const labelId = `enhanced-table-checkbox-${index}`;
+                            return (
+                                <TableRow
+                                    hover
+                                    role="checkbox"
+                                    aria-checked={isItemSelected}
+                                    tabIndex={-1}
+                                    key={index}
+                                    selected={isItemSelected}
+                                >
+                                    <TableCell
+                                        component="th"
+                                        id={labelId}
+                                        scope="row"
+                                        onClick={(event) => handleClick(event, row.name)}
+                                        sx={{ cursor: 'pointer' }}
                                     >
-                                        <TableCell
-                                            component="th"
-                                            id={labelId}
-                                            scope="row"
-                                            onClick={(event) => handleClick(event, row.name)}
-                                            sx={{ cursor: 'pointer' }}
+                                        <Typography
+                                            variant="subtitle1"
+                                            sx={{ color: theme.palette.mode === 'dark' ? 'grey.600' : 'grey.900' }}
                                         >
-                                            <Typography
-                                                variant="subtitle1"
-                                                sx={{ color: theme.palette.mode === 'dark' ? 'grey.600' : 'grey.900' }}
-                                            >
-                                                {' '}
-                                                #{row.id}{' '}
-                                            </Typography>
-                                        </TableCell>
-                                        <TableCell
-                                            component="th"
-                                            id={labelId}
-                                            scope="row"
-                                            onClick={(event) => handleClick(event, row.name)}
-                                            sx={{ cursor: 'pointer' }}
+                                            {' '}
+                                            #{row.id}{' '}
+                                        </Typography>
+                                    </TableCell>
+                                    <TableCell
+                                        component="th"
+                                        id={labelId}
+                                        scope="row"
+                                        onClick={(event) => handleClick(event, row.name)}
+                                        sx={{ cursor: 'pointer' }}
+                                    >
+                                        <Typography
+                                            variant="subtitle1"
+                                            sx={{ color: theme.palette.mode === 'dark' ? 'grey.600' : 'grey.900' }}
                                         >
-                                            <Typography
-                                                variant="subtitle1"
-                                                sx={{ color: theme.palette.mode === 'dark' ? 'grey.600' : 'grey.900' }}
-                                            >
-                                                {' '}
-                                                {row.name}{' '}
-                                            </Typography>
-                                        </TableCell>
-                                        <TableCell>{row.comment}</TableCell>
-                                        <TableCell align="center">
-                                            {row.status == 1 && <Chip label="Complete" size="small" chipcolor="success" />}
-                                            {row.status == 2 && <Chip label="Pending" size="small" chipcolor="orange" />}
-                                            {row.status == 3 && <Chip label="Processing" size="small" chipcolor="primary" />}
-                                        </TableCell>
-                                        <TableCell align="center" sx={{ pr: 3 }}>
-                                            <IconButton color="secondary" size="large" aria-label="edit" onClick={() => handleClickOpen(row)}>
-                                                <EditTwoToneIcon sx={{ fontSize: '1.3rem' }} />
-                                            </IconButton> 
-                                            {/* {newLocal} */}
-                                        </TableCell>    
-                                    </TableRow>
-                                );
-                            })}
-                        {emptyRows > 0 && (
-                            <TableRow
-                                style={{
-                                    height: 53 * emptyRows
-                                }}
-                            >
-                                <TableCell colSpan={6} />
-                            </TableRow>
-                        )}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            {/* table pagination */}
-            <TablePagination
-                rowsPerPageOptions={[5, 10, 25]}
-                component="div"
-                count={rows.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-            />
-             <FormDialog row={dialogRow} open={open} onClose={handleClose}></FormDialog>
-        </MainCard>
+                                            {' '}
+                                            {row.name}{' '}
+                                        </Typography>
+                                    </TableCell>
+                                    <TableCell>{row.comment}</TableCell>
+                                    <TableCell align="center">
+                                        {row.status == 1 && <Chip label="Complete" size="small" chipcolor="success" />}
+                                        {row.status == 2 && <Chip label="Pending" size="small" chipcolor="orange" />}
+                                        {row.status == 3 && <Chip label="Processing" size="small" chipcolor="primary" />}
+                                    </TableCell>
+                                    <TableCell align="center" sx={{ pr: 3 }}>
+                                        <IconButton color="secondary" size="large" aria-label="edit" onClick={() => handleClickOpen(row)}>
+                                            <EditTwoToneIcon sx={{ fontSize: '1.3rem' }} />
+                                        </IconButton>
+                                    </TableCell>
+                                </TableRow>
+                            );
+                        })}
+                    {emptyRows > 0 && (
+                        <TableRow
+                            style={{
+                                height: 53 * emptyRows
+                            }}
+                        >
+                            <TableCell colSpan={6} />
+                        </TableRow>
+                    )}
+                </TableBody>
+            </Table>
+        </TableContainer>
     );
 };
 
