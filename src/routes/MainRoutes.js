e873da8c12a94030a6ed0cd6668ed67cc900c2f2
Feng <hsufengs@gmail.com>
2024-04-27 18:12:18 +0800

    Add Customer Route

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -12,6 +12,7 @@ const DashboardAnalytics = Loadable(lazy(() => import('views/dashboard/Analytics
 // willWin routing
 const Material = Loadable(lazy(() => import('views/basic-setting/material/MaterialPage')));
 const Item = Loadable(lazy(() => import('views/basic-setting/item/ItemPage')));
+const Customer = Loadable(lazy(() => import('views/basic-setting/customer/CustomerPage')));
 
 // widget routing
 const WidgetStatistics = Loadable(lazy(() => import('views/widget/Statistics')));
@@ -151,6 +152,10 @@ const MainRoutes = {
         {
             path: '/basic-settings/item',
             element: <Item />
+        },
+        {
+            path: '/basic-settings/customer',
+            element: <Customer/>
         },
         {
             path: '/widget/statistics',
